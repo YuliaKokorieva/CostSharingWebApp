0e394d8e52b79d70f21fbda46b5efe77f5675a3f
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2021-11-04 23:10:09 +0200

    expense validation fixed

--- src/main/java/hh/swd20/CostSharing/domain/Expense.java
+++ src/main/java/hh/swd20/CostSharing/domain/Expense.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
@@ -17,7 +18,7 @@ public class Expense {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long expenseID;
-	@NotNull(message = "expense name cannot be null")
+	@NotBlank(message = "expense name cannot be null")
 	private String expenseName;
 	
 	@ManyToOne
@@ -29,7 +30,9 @@ public class Expense {
 	@JoinColumn(name="partID")
 	@JsonIgnoreProperties({"expenses", "trip", "participant"})
 	private Participant participant;
+	
 	@Positive(message="value cannot be negative or 0")
+	@NotNull
 	private Double expValue;
 	
 	public Expense() {
