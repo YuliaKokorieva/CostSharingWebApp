9cc825ab92d8dfe325ff17d50a198c57da83074d
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2021-11-13 22:20:54 +0200

    WebConfig corrected (REST, H2 + security)

--- src/main/java/hh/swd20/CostSharing/WebSecurityConfig.java
+++ src/main/java/hh/swd20/CostSharing/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import hh.swd20.CostSharing.web.UserDetailServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,6 +23,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable()
+	        .authorizeRequests()
+		        .antMatchers(HttpMethod.GET, "/**").permitAll()
+		        .antMatchers(HttpMethod.POST, "/**").permitAll()
+		        .and()
+		        .headers().frameOptions().disable()
+		        .and()
+		    .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+		        .and()
+		        .csrf().ignoringAntMatchers("/h2-console/**")
+		        .and()
+		        .headers().frameOptions().sameOrigin()
+		        .and()
 			.authorizeRequests()
 				.anyRequest().authenticated()
 				.and()
